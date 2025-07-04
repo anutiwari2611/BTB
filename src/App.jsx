03b819d78e1d4a901d8f388086a84350f0ade507
Anurg Tiwari <anuragt2611@gmail.com>
2025-07-04 23:39:54 +0530

    changes

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import "./Home.css";
+import Login from "./Login";
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <h1>Home</h1>
+    </div>
+  )
+}
+
+export default Home
