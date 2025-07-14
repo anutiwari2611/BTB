ea5f7bd75781bddd65e6cf9938f57610b55f490c
Anurag <as6400263@gmail.com>
2025-07-15 02:34:32 +0530

    fixing gitignore file

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,19 +4,22 @@ import { useState } from "react";
 import Form from "./Form";
 
 function Navbar() {
-  const [showForm,setShowForm]=useState(false)
-  return (<>
-    <nav>
-      <Link to="/" className="logo"><img src="./src/assets/images/Logo.png" alt="logo" /></Link>
-      <div>
-        <button onClick={()=>setShowForm(true)}>Sell</button>
-      </div>
-      <div className="nav-links">
-        <Link to="/login">Login</Link>
-        <Link to="/signup">Sign up</Link>
-      </div>
-    </nav>
-    {showForm && <Form onClose={()=>setShowForm(false)}/>}
+  const [showForm, setShowForm] = useState(false);
+  return (
+    <>
+      <nav>
+        <Link to="/" className="logo">
+          <img src="./src/assets/images/Logo.png" alt="logo" />
+        </Link>
+        <div>
+          <button onClick={() => setShowForm(true)}>Sell</button>
+        </div>
+        <div className="nav-links">
+          <Link to="/login">Login</Link>
+          <Link to="/signup">Sign up</Link>
+        </div>
+      </nav>
+      {showForm && <Form onClose={() => setShowForm(false)} />}
     </>
   );
 }
