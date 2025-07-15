21522a5cfe7ed95def9a549572a1702bbf885061
Anurg Tiwari <anuragt2611@gmail.com>
2025-07-15 22:44:48 +0530

    admin panel

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Admin from "./components/Admin";
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/admin" element={<Admin/>}/>
+        <Route path="/login" element={<Login />}/>
         <Route path="/signup" element={<Signup />} />
       </Routes>
     </div>
