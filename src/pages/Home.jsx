ea5560bfffe0bc9cf8b8a3b6a3c5a9b75aea8bd7
Anurg Tiwari <anuragt2611@gmail.com>
2025-08-16 23:42:12 +0530

    Removed duplicate files

--- src/pages/Home.jsx
+++
@@ -1,9 +0,0 @@
-const Home = () => {
-  return (
-    <div className="h-screen p-10">
-      <h1 className="text-3xl font-extrabold uppercase">Home</h1>
-    </div>
-  );
-};
-
-export default Home;
